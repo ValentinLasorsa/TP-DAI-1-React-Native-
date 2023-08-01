66f61f6494d3305904652b34f40e6e7790200fa8
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-01 09:01:59 -0300

    messi Campeon

--- Test-DAI-React-Native/App.js
+++ Test-DAI-React-Native/App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+
+
+const App = () => (
+  <View style={styles.container}>
+    <Image style={styles.image} source={require('./assets/Messi-mundial.png')} />
+      <Text style={styles.text}>CAMPEON</Text>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'space-around',
+  },
+  text: {
+    color: 'white',
+    fontSize: 68,
+    lineHeight: 84,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#73ACDF',
   },
 });
+
+export default App;
